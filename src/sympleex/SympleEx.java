4c4c6206eed9ade480949eeebefbfc62bd2ffc32
ArgoNesK <ArgoNesK@192.168.0.103>
2019-11-05 07:13:53 +0300

    Промежуточный результат

--- src/sympleex/SympleEx.java
+++ src/sympleex/SympleEx.java
@@ -5,29 +5,44 @@ import java.io.IOException;
 public class SympleEx {
 
     public static void main(String[] args) throws Exception {
-//        long startTime = System.nanoTime();
-//        //long estimatedTime = System.nanoTime() - startTime;
-//        long estimatedTime = System.nanoTime() * 60 * 10 ^ 9;
+        long startTime = System.nanoTime();
+        //long estimatedTime = System.nanoTime() - startTime;
+        long estimatedTime = System.nanoTime() * 60 * 10 ^ 9;
         Hello myHello = new Hello();
-
-        myHello.start();
-        for (int i = 0; i < 6; i++) {
-            Thread.sleep(6000);
-            System.out.print("World ");
-
-        }
-        if (myHello.isAlive()) {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("World ");
+            }
+        });
+        while (estimatedTime != 0) {
             try {
-                myHello.join();	
-            } catch (InterruptedException e) {
+                Thread.sleep(6000);
+                //myHello.start();
+
+                t1.start();
+            } catch (InterruptedException ex) {
+                break;
+
             }
 
-            System.out.println("Hello");
-        } else {
-            System.out.println("World");
         }
-        System.out.println("Finish");
 
+//        for (int i = 0; i < 6; i++) {
+//            Thread.sleep(6000);
+//            //System.out.print("World ");
+//
+//        }
+//        if (myHello.isAlive()) {
+//            try {
+//                myHello.join();
+//            } catch (InterruptedException e) {
+//            }
+//            System.out.println("Hello");
+//        } else {
+//            System.out.println("World");
+//        }
+//        System.out.println("Finish");
         try {
             System.in.read();
         } catch (IOException e) {
@@ -41,15 +56,15 @@ class Hello extends Thread {
 
     @Override
     public void run() {
-        while (true) {
+        //while (true) {
             System.out.print("Hello ");
-            try {
-                Thread.sleep(6000);
-            } catch (InterruptedException ex) {
-                break;
+//            try {
+//                Thread.sleep(6000);
+//            } catch (InterruptedException ex) {
+//                break;
 
-            }
-        }
+//            }
+        
 
     }
 }
